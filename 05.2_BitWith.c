b8e6e5cbf1d44244e9367c83526bcf006d229ddd
lvusyy <lvusyy@gmail.com>
2017-05-25 03:26:17 +0800

    file ,bitwith,chmod

---
+++ 05.2_BitWith.c
@@ -0,0 +1,53 @@
+//
+// Created by OthinkY
+//
+// 结构体 位宽
+//共用体 (同时只能 同时只能保证结构体内一个属性正常使用)
+
+#include <stdio.h>
+#include <string.h>
+
+int  main(int n, char *arrs[]){
+    struct bw{
+        //指定位宽 二进制的宽度 是3个 也就是说 存储的二进制宽度3 也就是:0b111  10进制就是7
+        unsigned int widthValidated:3;
+        // 存储的二进制宽度5 也就是:0b11111  10进制就是31
+        unsigned int hightValidated:5;
+    };
+
+    struct bw xx;
+                                //printf结果
+    xx.widthValidated=7;    //widthValidated 7
+    xx.hightValidated=32;   //widthValidated 0
+
+    printf("widthValidated %d\n",xx.widthValidated);
+    printf("widthValidated %d\n",xx.hightValidated);
+
+    //*****************************************************************
+    //共同体
+    // 这个结构体内,同时只能保证结构体内一个属性正常使用. 如果给person.age赋值之后 其它属性的值不能保证
+    union uperson{
+        int age;
+        float weight;
+        char name[50];
+    }person;
+
+    person.age=5;
+    printf("age:%d\n",person.age);
+    person.weight=5.22;
+    printf("weight:%f\n",person.weight);
+    strcpy(person.name,"花花虎");
+    printf("name:%s\n",person.name);
+    //
+    //age:5
+    //weight:5.220000
+    //name:花花虎
+    //
+    person.age=5;
+    person.weight=5.2;
+    strcpy(person.name,"小苗苗");
+    printf("age:%d,weight:%f,name:%s\n",person.age,person.weight,person.name);
+    //age:-393236251,weight:-5428492281570934657122304.000000,name:小苗苗
+
+    return 0;
+}
---
+++ 05_cTest.c
@@ -0,0 +1,65 @@
+//
+// Created by OthinkY on 5/24/2017.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+struct Books{
+    char title[50];
+    char author[30];
+    char subject[100];
+    int  book_id;
+} book;
+
+//只能在内部访问,
+static int sum_(int a,int* b);
+
+int   main(int argn, char *argv[])
+{
+//    int *pb=(int*)malloc(sizeof(int));
+// 	void *calloc(size_t nmemb，size_t size);
+    int *pb=(int*)calloc(1,sizeof(int));
+    *pb=2017;
+    printf("返回值:%d 指针b地址:%p,值:%d\n",sum_(11,pb),pb,*pb);
+    free(pb);
+
+    struct Books book1;
+
+    strcpy(book1.title,"我的小伙伴");
+    strcpy(book1.author,"lvusyy");
+    book1.book_id=1;
+    strcpy(book1.subject,"记录了小伙伴的消失录");
+
+    strcpy(book.title,"坏人,你跑啊!");
+    strcpy(book.author,"lvusyy");
+//    book.book_id=2;
+    int i=2;
+    memcpy(&book.book_id,&i, sizeof(i));
+    strcpy(book.subject,"小伙伴的成长见闻及发展");
+
+    printf("book1.title:%s\n",book1.title);
+    printf("book1.author:%s\n",book1.author);
+    printf("book1.book_id:%d\n",book1.book_id);
+    printf("book1.subject:%s\n",book1.subject);
+    //*******************************************
+    printf("*************************************\n");
+    printf("book2.title:%s\n",book.title);
+    printf("book2.author:%s\n",book.author);
+    printf("book2.book_id:%d\n",book.book_id);
+    printf("book2.subject:%s\n",book.subject);
+    return 0;
+}
+
+//返回 a+b的值 其中b是指针类型,函数执行完成后指针b也会有a+b的值
+static  int sum_(int a,int* b){
+    int tmp=0;
+    tmp=a+*b;
+//    *b+=a;
+
+    memcpy(b,&tmp, sizeof(tmp));
+
+    return tmp;
+}
